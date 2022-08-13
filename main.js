a1a41faeebe5bc3e9b8a0f9a480e2ba631c15254
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-08-13 13:21:32 -0500

    used .remove()instead of .toggle()for class hidden

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.querySelector('button').addEventListener('click', getFetch)
+//maybe add a function to refresh page to deal with toggle hidden issue or disable submit button at the end of the getFetch funcion and reenable when calender icon clicked
 // require('dotenv').config()
 // const api_keys = process.env.API_KEY
 
@@ -18,15 +19,15 @@ function getFetch(){
         // start with both image and iframe hidden then toggle hidden class for each outcome
         if( data.media_type === 'image' ){
           // show imgage
-          document.querySelector('img').classList.toggle('hidden')
+          document.querySelector('img').classList.remove('hidden')
           document.querySelector('img').src = data.hdurl
         }else if(data.media_type === 'video'){
           // show iframe
-          document.querySelector('iframe').classList.toggle('hidden')
+          document.querySelector('iframe').classList.remove('hidden')
           document.querySelector('iframe').src = data.url
         }
         // show explanation
-        document.querySelector('h3').classList.toggle('hidden')
+        document.querySelector('h3').classList.remove('hidden')
         document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
